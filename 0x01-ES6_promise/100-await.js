f53171face9cc7e9e753f0f978e163be34606b52
stephenkiai <stephenkiai23@gmail.com>
2023-11-28 20:14:17 +0300

    ES6 basics

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,18 +1,18 @@
 import { uploadPhoto, createUser } from './utils.js';
 
 export default async function asyncUploadUser() {
-    try {
-        const photoResponse = await uploadPhoto();
-        const userResponse = await createUser();
-    
-        // object with the responses
-        return {
-          photo: photoResponse,
-          user: userResponse,
-        };
-      } catch (error) {
-        // Handle any errors
-        console.error('Error in asyncUploadUser:', error);
-        throw error;
-      }
+  try {
+    const photoResponse = await uploadPhoto();
+    const userResponse = await createUser();
+
+    // object with the responses
+    return {
+        photo: photoResponse,
+        user: userResponse,
     };
+    } catch (error) {
+    // Handle any errors
+    console.error('Error in asyncUploadUser:', error);
+    throw error;
+    }
+};
